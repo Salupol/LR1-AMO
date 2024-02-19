74f1bf225b43a4992ce56b127b80042b2ba584b8
Pavlo <44544644+Salupol@users.noreply.github.com>
2024-02-19 11:35:07 +0200

    5

--- function_value.py
+++ function_value.py
@@ -2,16 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Initial data
 x_init = [-0.11, 0.11, 0.24, 0.36, 0.57, 0.66, 0.89, 1.1, 1.39, 1.6]
 y_init = [0.7764, 1.2382, 1.5394, 1.8374, 2.4101, 2.6780, 3.4356, 4.2396, 5.5884, 6.7991]
 x_args = np.arange(0, 1.5 + 0.01, 0.01)
 
-# Function values
 y_args_formula = [3 ** i + np.sin(i) for i in x_args]
 
 
-# Interpolation functions
 def cubic_interpolation(x, y, x_args):
     def find_closest_values(x_input):
         for i in range(3, len(x)):
@@ -39,11 +36,9 @@ def lagrange_interpolation(x, y, x_args):
     return y_args_lagrange
 
 
-# Interpolation results
 y_args_cubic = cubic_interpolation(x_init, y_init, x_args)
 y_args_lagrange = lagrange_interpolation(x_init, y_init, x_args)
-
-# Plotting
+plt.figure(figsize=(10, 10), dpi=300)
 plt.plot(x_args, y_args_formula, linewidth=3, label="initial formula")
 plt.plot(x_args, y_args_cubic, linewidth=3, label="cubic interpolation")
 plt.plot(x_args, y_args_lagrange, linewidth=3, label="lagrange interpolation")
@@ -51,7 +46,6 @@ plt.plot(x_init, y_init, 'ro', label="initial values")
 plt.legend()
 plt.show()
 
-# Results output
 df = pd.DataFrame({"x": x_args, "y_formula": y_args_formula, "y_cubic": y_args_cubic, "y_lagrange": y_args_lagrange})
 pd.set_option('display.max_rows', None)
 print(df)
